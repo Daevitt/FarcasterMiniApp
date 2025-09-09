74ebd33879b10fee4c967d097d61b43a36d409ff
Daevitt <david.eternalsgaming@gmail.com>
2025-09-09 11:31:20 -0500

    Create route.ts

---
+++ app/api/auth/[...nextauth]/route.ts
@@ -0,0 +1,44 @@
+import NextAuth from "next-auth"
+import { NextAuthOptions } from "next-auth"
+import { cookies } from "next/headers"
+
+const authOptions: NextAuthOptions = {
+  providers: [
+    {
+      id: "farcaster",
+      name: "Farcaster",
+      type: "oauth",
+      clientId: process.env.NEYNAR_CLIENT_ID!,
+      clientSecret: process.env.NEYNAR_CLIENT_SECRET!,
+      wellKnown: "https://api.neynar.com/v2/oauth/.well-known/openid-configuration",
+      authorization: { params: { scope: "openid offline_access" } },
+      idToken: true,
+      profile(profile) {
+        return {
+          id: profile.sub,
+          name: profile.username,
+          image: profile.pfp_url,
+          fid: profile.fid,
+        }
+      },
+    },
+  ],
+  callbacks: {
+    async jwt({ token, user, account }) {
+      if (user) {
+        token.fid = user.fid
+      }
+      return token
+    },
+    async session({ session, token }) {
+      if (token?.fid) {
+        session.user.fid = token.fid
+      }
+      return session
+    },
+  },
+  secret: process.env.NEXTAUTH_SECRET,
+}
+
+const handler = NextAuth(authOptions)
+export { handler as GET, handler as POST }
