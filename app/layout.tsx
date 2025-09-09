31899b416318ad2276adb95abdfb7b5b79152eb0
Daevitt <david.eternalsgaming@gmail.com>
2025-09-09 13:29:20 -0500

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,12 +1,17 @@
-'use client'
-
 import { SessionProvider } from "next-auth/react"
+import "./globals.css"
 
-export default function RootLayout({ children }: { children: React.ReactNode }) {
+export default function RootLayout({ 
+  children 
+}: { 
+  children: React.ReactNode 
+}) {
   return (
     <html lang="en">
       <body>
-        <SessionProvider>{children}</SessionProvider>
+        <SessionProvider>
+          {children}
+        </SessionProvider>
       </body>
     </html>
   )
