3d734e29e71dd9410ce38d36f4a9a477a1908f80
Daevitt <david.eternalsgaming@gmail.com>
2025-09-09 13:27:25 -0500

    Update route.ts

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,17 +1,3 @@
-import NextAuth from "next-auth"
+import { handlers } from "@/auth"
 
-const authHandler = NextAuth({
-  providers: [
-    {
-      id: "neynar",
-      name: "Neynar",
-      type: "oidc",
-      wellKnown: "https://api.neynar.com/.well-known/openid-configuration",
-      clientId: process.env.NEYNAR_CLIENT_ID!,
-      clientSecret: process.env.NEYNAR_CLIENT_SECRET!,
-    },
-  ],
-  secret: process.env.NEXTAUTH_SECRET,
-})
-
-export { authHandler as GET, authHandler as POST }
+export const { GET, POST } = handlers
