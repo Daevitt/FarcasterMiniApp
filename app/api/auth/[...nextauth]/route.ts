d13373b61764c28015903cec597a47f2e6091581
Daevitt <david.eternalsgaming@gmail.com>
2025-09-09 12:37:48 -0500

    Update route.ts

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,7 +1,7 @@
 // app/api/auth/[...nextauth]/route.ts
 import NextAuth from "next-auth"
 import type { NextAuthConfig } from "next-auth"
-import OIDCProvider from "next-auth/providers/oidc"
+import OIDCProvider from "@auth/oidc-provider"
 
 // Configuración
 const authConfig: NextAuthConfig = {
