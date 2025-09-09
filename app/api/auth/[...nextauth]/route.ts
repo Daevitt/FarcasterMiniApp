3692c139c5666ed4157dca3c7c9f047b8a5538d2
Daevitt <david.eternalsgaming@gmail.com>
2025-09-09 12:54:07 -0500

    Update route.ts

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,12 +1,12 @@
 import NextAuth from "next-auth"
 import type { AuthOptions } from "next-auth"
 
-export const authOptions: AuthOptions = {
+const authOptions: AuthOptions = {
   providers: [
     {
       id: "neynar",
       name: "Neynar",
-      type: "oidc", // aquí indicamos que es OpenID Connect
+      type: "oidc",
       wellKnown: "https://api.neynar.com/.well-known/openid-configuration",
       clientId: process.env.NEYNAR_CLIENT_ID!,
       clientSecret: process.env.NEYNAR_CLIENT_SECRET!,
